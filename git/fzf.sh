9429365aee9155c03a619250bb641da933b85299
Matt Shen <synmantics@gmail.com>
2016-11-19 09:26:13 -0800

    Add git fzf and summary commands

--- git/fzf.sh
+++ git/fzf.sh
@@ -1,6 +1,16 @@
 # GIT heart FZF
 # -------------
 
+gitfzf() {
+  cat <<\EOF
+gf\tfiles
+gb\tbranches
+gt\ttags
+gh\thistory
+gr\tgraph
+EOF
+}
+
 is_in_git_repo() {
   git rev-parse HEAD > /dev/null 2>&1
 }
